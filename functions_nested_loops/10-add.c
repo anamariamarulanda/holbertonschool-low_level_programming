ffdb256d45989f82a37e0697d654b266b309a9fb
Ana María Marulanda <5849@holbertonstudents.com>
2022-10-14 16:55:19 -0700

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+/*
+ *  *=================================================================
+ *  * PROYECT C - Functions, nested loops
+ *  * TASK 10 :   a + b
+ *  *=================================================================
+ *  * AUTHOR : ANA MARIA MARULANDA GALLEGO
+ *  *=================================================================
+ */
+
+#include "main.h"
+
+/**
+ ** add - Adds two values
+ ** @n1: The first number to be computed
+ ** @n2: The second number to be computed
+ **
+ ** Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
