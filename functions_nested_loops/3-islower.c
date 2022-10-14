89558e1c9e4eaaf9b96974127b9a5fe481813b75
Ana María Marulanda <5849@holbertonstudents.com>
2022-10-14 14:02:05 -0700

    3-islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+/*
+ *===================================================================
+ * PROYECT C - Functions, nested loops
+ * TASK 3 : islower
+ *===================================================================
+ * AUTHOR : ANA MARIA MARULANDA GALLEGO
+ *===================================================================
+ */
+
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 for lowercase character or 0 anything else
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
