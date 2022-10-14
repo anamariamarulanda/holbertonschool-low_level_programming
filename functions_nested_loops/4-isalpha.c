5917e3f1dac70cf8997600aaa6902dc6d5139976
Ana María Marulanda <5849@holbertonstudents.com>
2022-10-14 14:57:32 -0700

    4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+/*
+ **===================================================================
+ ** PROYECT C - Functions, nested loops
+ ** TASK 3 : islower
+ **===================================================================
+ ** AUTHOR : ANA MARIA MARULANDA GALLEGO
+ **===================================================================
+ */
+
+#include "main.h"
+
+/**
+ * * _isalpha - checks for alphabetic character.
+ * * @c: To prove
+ * * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
