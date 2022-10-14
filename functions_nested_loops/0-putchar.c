4acd55a77881ad28d990c12adebf1f67985cc4dd
Ana María Marulanda <5849@holbertonstudents.com>
2022-10-14 10:53:44 -0700

    main.h

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+/*
+ * ==================================================================
+ * PROYECT C - Functions, nested loops
+ * TASK 0 : _putchar
+ * ==================================================================
+ * AUTHOR : ANA MARIA MARULANDA GALLEGO
+ * ==================================================================
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
