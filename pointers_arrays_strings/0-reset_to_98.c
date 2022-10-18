b951914994cbdb2d7c761b92867d4d5a0218c86a
Ana María Marulanda <5849@holbertonstudents.com>
2022-10-18 13:41:04 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+/*
+ *  * ==================================================================
+ *  * PROYECT C - Functions, nested loops
+ *  * TASK 0 : _putchar
+ *  * ==================================================================
+ *  * AUTHOR : ANA MARIA MARULANDA GALLEGO
+ *  * ==================================================================
+ */
+
+#include "main.h"
+
+/**
+ ** reset_to_98 - takes a pointer to an int as parameter and
+ ** updates the value it points to to 98.
+ ** @n: pointer to n
+*/
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
+
+
